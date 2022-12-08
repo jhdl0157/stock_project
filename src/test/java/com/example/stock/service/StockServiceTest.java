aba20a6f97428c85bd772e9dac6cd5462b4453db
jhdl0157 <jhdl0157@naver.com>
2022-12-08 12:18:18 +0900

    Test : 100개 동시 요청 테스트

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class StockServiceTest {
@@ -38,5 +42,28 @@ class StockServiceTest {
 
         assertEquals(99,stock.getQuantity());
     }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount=100;
+        ExecutorService executorService= Executors.newFixedThreadPool(32);
+        CountDownLatch latch=new CountDownLatch(threadCount);
+        for(int i=0;i<threadCount;i++){
+            executorService.submit(()->{
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        var stock=stockRepository.findById(1L).orElseThrow();
+
+        //100 -100 =0
+        assertEquals(0L,stock.getQuantity());
+    }
+
 
 }
